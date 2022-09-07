6acb8928e7aa73a79d4d81f29a2ae5cd5da7ac7f
kiqueangulo <kiqueangulo@gmail.com>
2022-09-07 19:59:48 -0400

    Finish 5th part

--- Bonus/5.py
+++ Bonus/5.py
@@ -1,1 +1,21 @@
-# Write your solution for algorithm 5 below
+def sum_to(numbers, target):
+    numbers.sort()
+    first = 0
+    last = len(numbers) - 1
+
+    while first < last:
+        sum = numbers[first] + numbers[last]
+
+        if sum == target:
+            return numbers[first], numbers[last]
+        
+        if sum > target:
+            last -= 1
+        else:
+            first += 1
+    
+    return 'There are no terms for that sum.'
+
+lst_example = [4, 7, 2, 8, 6, 1]
+
+print(sum_to(lst_example, 3))
