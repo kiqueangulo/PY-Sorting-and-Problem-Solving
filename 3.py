6986f8389d77459112f1c9e3a8f847f97a4c32b5
kiqueangulo <kiqueangulo@gmail.com>
2022-09-07 19:31:42 -0400

    Finish third part

--- 3.py
+++ 3.py
@@ -1,1 +1,6 @@
-# Write your solution for algorithm 3 below
+def sort_and_reverse(lst):
+    return sorted(lst, reverse = True)
+
+lst_example = [4, 7, 2, 8, 6, 1]
+
+print(sort_and_reverse(lst_example))
